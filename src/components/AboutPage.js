53c9bd11af47a7b06e56d7e43aee4acee1a95e44
wasim-bachani <72779988+wasim-bachani@users.noreply.github.com>
2020-10-28 22:47:56 -0700

    feat(header): added first react components and header

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//this component is created using a class
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+      </>
+    );
+  }
+}
+
+export default AboutPage;
